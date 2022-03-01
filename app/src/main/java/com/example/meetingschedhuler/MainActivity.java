e9ed29ddafd91fd73f654f2025477de25c94374e
DEY RAJIB <rade002@ess.ais.ac.nz>
2022-03-02 00:12:20 +1300

    Navigation Drawer implemented

--- app/src/main/java/com/example/meetingschedhuler/MainActivity.java
+++ app/src/main/java/com/example/meetingschedhuler/MainActivity.java
@@ -1,14 +1,80 @@
 package com.example.meetingschedhuler;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new AllContactFragment ()).commit();
+            navigationView.setCheckedItem(R.id.all_contacts);
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.all_contacts:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AllContactFragment()).commit();
+                break;
+            case R.id.important_contacts:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ImportantContactFragment()).commit();
+                break;
+            case R.id.favourite_contacts:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new FavouriteContactFragment()).commit();
+                break;
+                /*
+            case R.id.nav_share:
+                Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_send:
+                Toast.makeText(this, "Send", Toast.LENGTH_SHORT).show();
+                break;
+                */
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
